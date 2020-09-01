1dcfd0d443a6e4630affd2ae1e0607a3fdab4f39
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-01 08:14:56 +0000

    Deploy shinee111111/shoushouship to github.com/shinee111111/shoushouship.git:gh-pages

---
+++ precache-manifest.7e1a093c830a32d8d60daef74bb81985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf52c98d068f22dcb260c5bcffc772aa",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ea376b925006768110ef.bundle.js"
+  },
+  {
+    "url": "runtime~main.ea376b925006768110ef.bundle.js"
+  },
+  {
+    "revision": "79f08ea6aa3d29f2a216",
+    "url": "static/css/main.e7f53f4a.chunk.css"
+  },
+  {
+    "revision": "6598e23af112bf7cb045",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.ea376b925006768110ef.bundle.js"
+  },
+  {
+    "url": "vendors~main.ea376b925006768110ef.bundle.js.LICENSE.txt"
+  }
+]);
